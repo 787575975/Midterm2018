96d1c54310a9e4cc99e0cf7e1e6b6ae7cbdf6266
DOLADAOUD <doladaoud710@gmail.comIbrahim-99git config  --global  user.email doladaoud710@gmail.com>
2018-12-23 16:29:31 -0500

    math.problems

--- src/math/problems/LowestNumber.java
+++ src/math/problems/LowestNumber.java
@@ -1,18 +1,20 @@
 package math.problems;
 
-public class LowestNumber{
+public class LowestNumber {
+	public static int findLowestNumber(int[] array) {
 
-	public static void main(String[] args) {
+
 		int min = array[0];
-		for (int i=1; i<array.lenght; i++){
-			if (array[i]<min);
-			min= array[i];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < min) min= array[i] ;
 
 		}
 		System.out.println(min);
 		return min;
 
 	}
+
+	public static void main(String[] args) {
 
 		/*
 		 * Write java solution to find the lowest number from this array.
@@ -21,26 +23,27 @@ public class LowestNumber{
 		 */
 
 
-		int array[]= new int[]211,110,99,34,67,89,67,456,321,456,78,90,45,32,56,78,90,54,32,123,67,5,679,54,32,65};
+		int array []= new int[]{211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123, 67, 5, 679, 54, 32, 65};
 
 
-	//find lowest number from the array
-	findLowestNumber(array);
+		//find lowest number from the array
+		findLowestNumber(array);
 
-	ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-	List<String> lowestValue = new ArrayList<String>();
+		/*ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
+		List<String> lowestValue = new ArrayList<String>();
 		try {
-		connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
-		lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
+			connectToSqlDB.insertDataFromArrayToSqlTable(array, "tbl_lowestNumber", "column_lowestNumber");
+			lowestValue = connectToSqlDB.readDataBase("tbl_lowestNumber", "column_lowestNumber");
 
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-		System.out.println("Data is reading from the Table (tbl_primenumber) and displaying to the console");
-		for(String st:lowestValue){
-		System.out.println(st);
-	}
-}
+		} catch (Exception e) {
+			e.printStackTrace();*/
+		/*}
+		System.out.println("Data is reading from the Table (tbl_prime number) and displaying to the console");
+		for (String st : lowestValue) {
+			System.out.println(st);
+*/
 
+		}
+	}
 
 
--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,8 +1,40 @@
 package math.problems;
 
+import org.testng.Assert;
+
 public class UnitTestingMath {
     public static void main(String[] args) {
-        //Apply Unit testing into each classes and methods in this package.
 
-    }
-}
+
+                //Apply Unit testing into each classes and methods in this package.
+
+                // testing for factorial
+                Assert.assertEquals(120, Factorial.factorial(5)); // this for factorial
+
+                // testing for find lowest difference
+
+                int [] array1 = {30,12,5,9,2,20,33,1};
+                int [] array2 = {18,25,41,47,17,36,14,19};
+                Assert.assertEquals(1,FindLowestDifference.lowestDiff(array1, array2));
+
+                // testing for find missing number
+                int[] array3 = {1, 2, 3, 4, 5, 6, 7, 8, 10};
+                Assert.assertEquals(9,FindMissingNumber.findMissingNumber(array3,10));
+
+                // testing for find lowest number
+                int[] array4 = {1, 36, 2, 5, 8, 9, 78};
+                Assert.assertEquals(1, LowestNumber.findLowestNumber(array4));
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
