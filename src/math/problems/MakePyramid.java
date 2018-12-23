d0130ba61d377640b824c60b205cdfe7a68bfbe8
DOLADAOUD <doladaoud710@gmail.comIbrahim-99git config  --global  user.email doladaoud710@gmail.com>
2018-12-23 14:23:44 -0500

    MidtermNovember2018

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -2,18 +2,10 @@ package math.problems;
 
 public class MakePyramid {
     public static void main(String[] args) {
-
-        /*   Implement a large Pyramid of stars in the screen with java.
-
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
-
-        */
+    }
+}
 
 
-        }
-  }
+
+
+
