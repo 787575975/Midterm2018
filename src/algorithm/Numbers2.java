d0e9e1a909d704eec56ea4fdb1a6d278549d038a
DOLADAOUD <doladaoud710@gmail.comIbrahim-99git config  --global  user.email doladaoud710@gmail.com>
2018-12-23 11:21:47 -0500

    definition

---
+++ src/algorithm/Numbers2.java
@@ -0,0 +1,30 @@
+package algorithm;
+
+import databases.ConnectDB;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class Numbers2 {
+    public static void main(String[] args) {
+        //   Map<String, List<String>> num = new int[100];
+
+
+      /*  Random ran = new Random();
+        for(int i=0; i<num.length; i++){
+            num[i] = ran.nextInt(100);
+        }
+        ConnectDB con = new ConnectDB();
+Sort algo = new Sort();
+algo.selectionSort(num);
+long selectionsSortExecutionTime = algo.executionTime;
+        System.out.println("Execution time"+num.length+ " number in selection sort "+selectionsSortExecutionTime);
+        con.InsertDataFromArrayListToMySql(num,"selectionSort","SortingNumbers");
+*/
+
+
+
+    }
+}
+
